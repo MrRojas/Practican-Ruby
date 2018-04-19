f028cc7abc6bb5c43a2f34db5f18f3f6855e83d5
= <=>
2018-04-19 00:44:26 -0400

    primeros pasos

---
+++ Clase_1/main.rb
@@ -0,0 +1,17 @@
+
+# muestra en pantalla 
+puts "Ingresa tu Nombre"
+
+# lectura por teclado
+nombre = get.chomp
+
+# muestra en pantalla 
+puts "Ingresa tu Edad"
+
+# lectura por teclado
+edad = get.chomp.to_i 
+
+# muestra el valor de la variable nombre
+puts "Nombre = #{nombre}"
+# muestra el valor de la variable edad
+puts "edad = #{edad}"
