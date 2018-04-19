fb93250c121cbd7cb3c3c0aa43698f33b04a6fd3
= <=>
2018-04-19 14:24:12 -0400

    funciones basicas

---
+++ Clase_5/main.rb
@@ -0,0 +1,53 @@
+# Funciones 
+
+def cerenis()
+
+	puts "Hola Cerenis"
+
+end 
+
+# invocar a la funcion si parametros 
+cerenis
+
+
+# funcion con parametro
+def armando( valor = "Armando Rojas" )
+
+	puts "hola #{valor}"
+	
+end
+
+armando
+
+armando("Leonardo Jose")
+
+# retornado valor 
+
+def x2(valor = 0)
+
+	return valor * valor
+
+end
+
+# 16
+puts x2(4)
+
+
+def saludar nombre
+
+	puts "¡Hola! #{nombre}"
+	
+end
+
+
+def saludarNP nombre , apellido
+
+	puts "¡Hola! #{nombre} #{apellido}"
+	
+end
+
+saludar "Cerenis Cabrera"
+
+saludarNP "Armando" , "Rojas"
+
+# usango argumentos 
